c7f8a9c54a39610dc675a23835083955ed2d9b1b
BryanC714 <165086565+BryanC714@users.noreply.github.com>
2025-05-28 20:00:01 -0500

    Implementacion de usecases

---
+++ lib/src/app/features/notes/domain/usecases/get_notes_usecase.dart
@@ -0,0 +1,18 @@
+import '../models/note_model.dart';
+import '../repositories/note_repository.dart';
+import 'package:flutter_clean_arquitecture/src/common/network/error/api_error.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:injectable/injectable.dart';
+//@injectable: permite que la clase sea intyectada automaticamente
+//utilizando el paquete get_it
+//la anotacion injectable permite que esta clase sea registrada e inyectada automaticamente en otras clases
+//No se sabe si los datos vienen de API, SQLLite, etc
+@injectable
+class GetNotesUsecase {
+  final NoteRepository _repository;
+  GetNotesUsecase(this._repository);
+
+  Future<Either<ApiError, List<NoteModel>>> call() async {
+    return await _repository.getNotes();
+  }
+}
