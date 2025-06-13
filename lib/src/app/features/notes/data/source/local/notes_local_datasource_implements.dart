fa14ef32e1a4954b3ee8b10d819a71eae591f0cd
BryanC714 <165086565+BryanC714@users.noreply.github.com>
2025-06-13 17:01:06 -0500

    notes_local_datasource_implements

--- lib/src/app/features/notes/data/source/local/notes_local_datasource_implements.dart
+++ lib/src/app/features/notes/data/source/local/notes_local_datasource_implements.dart
@@ -1,18 +1,35 @@
 import 'package:flutter_clean_arquitecture/src/common/network/api/local_api_client.dart';
+import 'package:flutter_clean_arquitecture/src/common/utils/constants/preferences_keys.dart';
 import 'package:injectable/injectable.dart';
 import 'notes_local_datasource.dart';
 
-class NotesLocalDatasourceImplements implements NotesLocalDatasource{
+@Injectable(as: NotesLocalDataSource)
+class NotesLocalDataSourceImplements implements NotesLocalDataSource {
+  final LocalApiClient _localApiClient;
+  NotesLocalDataSourceImplements(this._localApiClient);
   @override
-  Future<void> decrementFavouriteCount() {
-    // TODO: implement decrementFavouriteCount
-    throw UnimplementedError();
+  Future<void> decrementFavouriteCount() async{
+    try {
+      var currentCount = _localApiClient.getint(PreferencesKeys.favoriteCount);
+      currentCount--;
+      await _localApiClient.setInt(PreferencesKeys.favoriteCount, value: currentCount);
+    } catch (_) {
+      //Atrapar cualquier excepción generada en el bloque try
+      //rethrow lanza la misma excepción al sitio donde se invoque al metodo
+      rethrow;
+    }
   }
 
   @override
-  Future<void> incrementFavouriteCount() {
-    // TODO: implement incrementFavouriteCount
-    throw UnimplementedError();
+  Future<void> incrementFavouriteCount() async{
+    try {
+      var currentCount = _localApiClient.getint(PreferencesKeys.favoriteCount);
+      currentCount++;
+      await _localApiClient.setInt(PreferencesKeys.favoriteCount, value: currentCount);
+    } catch (_) {
+      //Atrapar cualquier excepción generada en el bloque try
+      //rethrow lanza la misma excepción al sitio donde se invoque al metodo
+      rethrow;
+    }
   }
-
 }
